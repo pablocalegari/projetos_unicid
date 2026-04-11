b3119cd2baff3adabe0faaf0a47a587ffebea87f
pablocalegari <pabloantoniocalegari@gmail.com>
2026-04-10 21:39:38 -0300

    Create insertion_sort.c

---
+++ OrdenacaodePacientes/insertion_sort.c
@@ -0,0 +1,14 @@
+#include "insertion_sort.h"
+#include <string.h>
+
+void insertionSort(Paciente v[], int esq, int dir) {
+    int i, j;
+    Paciente aux;
+
+    for (i = esq + 1; i <= dir; i++) {
+        aux = v[i];
+        j = i - 1;
+
+while()
+    }
+}
